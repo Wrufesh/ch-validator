7d1b7e5382230850e0b5d27b075d0213e3da017f
Wrufesh <wrufesh@gmail.com>
2016-03-15 14:12:19 +0545

    can get text input from regular form

--- myscript.js
+++ myscript.js
@@ -1,2 +1,16 @@
-console.log('Hello World');
-alert("Hello World");
+console.log('Hello World3');
+window.onload = function() {
+	var str = $( "form" ).serialize();
+	console.log(str);
+	$( "form" ).on( "submit", function( event ) {
+	  event.preventDefault();
+	  console.log("Inside submit event")
+	  console.log( $( "form input[type=text]" ).serialize() );
+	});
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }};
